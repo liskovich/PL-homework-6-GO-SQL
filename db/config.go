dc5ffacb45db2748a36e491aff979b7ee65f47b1
liskovich <ricardsliskovskis@gmail.com>
2023-11-28 10:47:00 +0200

    chore: add seed data

--- db/config.go
+++ db/config.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 
+	"example.com/api/model"
 	_ "github.com/lib/pq"
 )
 
@@ -29,9 +31,20 @@ func ConnectDB() *sql.DB {
 		log.Fatal(err)
 	}
 
-	createTables(db)
-	seedData(db)
+	if isDBEmpty(db) {
+		createTables(db)
+		seedData(db)
+	}
 	return db
+}
+
+func isDBEmpty(db *sql.DB) bool {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM users;").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return count == 0
 }
 
 func createTables(db *sql.DB) {
@@ -44,6 +57,34 @@ func createTables(db *sql.DB) {
 }
 
 func seedData(db *sql.DB) {
-	// TODO: add seed data if needed
-	// Example: db.Exec("INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@example.com', 'password')")
+	// add some default user
+	db.Exec("INSERT INTO users (name, email, password) VALUES ('Admin', 'admin@example.com', 'password_hash');")
+	row := db.QueryRow(SelectUserByEmailQuery, "admin@example.com")
+	var user model.User
+	err := row.Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+	)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		// insert some beers
+		b1 := fmt.Sprintf(
+			"INSERT INTO beers (name, description, thumbnail, author_id) VALUES ('Heineken', 'There is more behind the star', 'https://img.merkandi.com/imgcache/420x320/images/offer/2019/12/05/heineken-lager-1575545711-1575545939.jpeg', %v);",
+			user.ID,
+		)
+		b2 := fmt.Sprintf(
+			"INSERT INTO beers (name, description, thumbnail, author_id) VALUES ('Carlsberg', 'Probably the best beer in the world', 'https://auziliquor.com.au/cdn/shop/products/Carlsberg-elephant-premium-strong-beer_900x.jpg?v=1650449387', %v);",
+			user.ID,
+		)
+		b3 := fmt.Sprintf(
+			"INSERT INTO beers (name, description, thumbnail, author_id) VALUES ('Cesu PREMIUM original', 'Distinguished by its soft golden color and sparkling refreshment', 'https://www.cesualus.lv/wp-content/uploads/2016/02/CesuPremiumOriginalaisBotPel.jpg', %v);",
+			user.ID,
+		)
+		db.Exec(b1)
+		db.Exec(b2)
+		db.Exec(b3)
+	}
 }
